0541b8106677c75fbd91756cc969bf436dadef89
PrismTrix <prismtrix1@gmail.com>
2023-05-23 09:53:35 -0300

    Changelogs

---
+++ Events/ChangeLogs.js
@@ -0,0 +1,1 @@
+console.log(`${chalk.green('[ CHANGELOGS ]')} As Seguintes Mudanças Foram Aplicadas: \n - Agora Possuimos Suporte Para Módulos Que Serão Disponibilizados Em Breve\n - A Versão Do Node Foi Atualizada Para A Mais Recente\n - Versão Atual: 1.4.3\n`)
