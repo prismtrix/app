64024e500cdbf62fd4b016ca2fdd9a6fad1ed9d3
PrismTrix <prismtrix1@gmail.com>
2023-05-27 19:56:49 -0300

    Bug Fix

--- Events/ChangeLogs.js
+++ Events/ChangeLogs.js
@@ -1,5 +1,5 @@
 console.log(
   `${chalk.green(
     "[ CHANGELOGS ]"
-  )} As Seguintes Mudanças Foram Aplicadas: \n - Nova Função Adicionada: Client.slowType(String)\n - Versão Atual: 1.6\n`
+  )} As Seguintes Mudanças Foram Aplicadas: \n - Nova Função Adicionada: Client.slowType(String)\n - Versão Atual: 1.6.1\n`
 );
